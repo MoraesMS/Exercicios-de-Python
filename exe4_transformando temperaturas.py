9007653fefafe62156dead5ba3f2505bca2ebc2c
Marcelo Santana Moraes <msmoraes.1@gmail.com>
2022-09-16 14:35:37 -0300

    deixando mais simples

--- exe4_transformando temperaturas.py
+++ exe4_transformando temperaturas.py
@@ -1,15 +1,12 @@
-while True:
-    print('Qual temperatura deseja ver:\n'
+print('Qual temperatura deseja ver:\n'
           '[1] ºC para ºF\n'
           '[2] ºF para ºC')
-    resp = int(input('Resposta: '))
-    if resp == 1:
-        tc = float(input('Digite a temperatura em ºC: '))
-        tf = tc * 9 / 5 +32
-        print(f'A temperatura de {tc}ºC, são {tf:.1f}ºF.')
-        break
-    else:
-        tf = float(input('Digite a temperatura em ºF: '))
-        tc = (tf - 32) / 1.8
-        print(f'A temperatura de {tf}ºF, são {tc:.1f}ºC.')
-        break
+resp = int(input('Resposta: '))
+if resp == 1:
+    tc = float(input('Digite a temperatura em ºC: '))
+    tf = tc * 9 / 5 +32
+    print(f'A temperatura de {tc}ºC, são {tf:.1f}ºF.')
+else:
+    tf = float(input('Digite a temperatura em ºF: '))
+    tc = (tf - 32) / 1.8
+    print(f'A temperatura de {tf}ºF, são {tc:.1f}ºC.')
